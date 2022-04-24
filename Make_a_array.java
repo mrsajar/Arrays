d09db977db17641c1f0a5ee9459cff5f4bb401a2
mrsajar <mssaifi386@gmail.com>
2022-04-24 12:38:11 -0700

    Traversal of array

---
+++ Make_a_array.java
@@ -0,0 +1,9 @@
+public class Make_a_array {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,2};
+        System.out.println("the all values of array..");
+        for(int i=0;i< arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
