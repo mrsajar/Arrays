7ed536670b889eb9d7d866e27b6bf1a16128989e
mrsajar <mssaifi386@gmail.com>
2022-05-01 03:34:04 -0700

    Swipe kth element in array

---
+++ Swipe_kth_element.java
@@ -0,0 +1,20 @@
+public class Swipe_kth_element {
+    public static void main(String[] args) {
+        int array[]={1,2,3,4,5,6,7,8,9};
+        int n= array.length;
+        int k=3;
+        System.out.println("Before swipe kth element");
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+        //After swipe the element
+        int temp=array[k-1];
+        array[k-1]=array[n-k];
+        array[n-k]=temp;
+        System.out.println("After swipe the kth element in array..");
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+
+    }
+}
